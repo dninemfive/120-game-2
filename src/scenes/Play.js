8a1013f156d1694c57a9ac10cd6c795da0d6e9ac
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-05-13 10:56:14 -0700

    background now in game

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,11 +4,11 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image("background", "assets/TrailingLightsProtoBackground.png");
     }
 
     create() {
-
+        this.background = this.add.sprite(game.config.width / 2, game.config.height / 2,"background").setOrigin(0.5,0.5).setDepth(-2);
     }
 
     update() {
