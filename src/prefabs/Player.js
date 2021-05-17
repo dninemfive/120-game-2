5696a9303e29abddd6451ddee98846e2267fccf0
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-05-16 20:16:17 -0700

    remove debug print

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -47,7 +47,6 @@ class Player extends Phaser.GameObjects.Sprite {
         if(this.pos[name] < (-backgroundSize + screenSize)){
             this[name] = this.pos[name] - (screenSize / 2) + backgroundSize;
         } else if(this.pos[name] > (backgroundSize - screenSize)){
-            console.log("this.pos." + name + ": " + this.pos[name] + " sd: " + (screenSize / 2) + " bd: " + backgroundSize + " res: " + (this[name] = this.pos[name] + (screenSize / 2) - backgroundSize));
             this[name] = this.pos[name] + (1.5 * screenSize) - backgroundSize;
         } else{
             this[name] = screenSize / 2;
